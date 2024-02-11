190d8f18ae90699ac63be9f111f430c71d7b838b
McPerry11 <mack.perry.co@gmail.com>
2024-02-11 18:40:08 +0800

    V0.1.0 - Added login elements.

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+
+      <Text style={styles.title}>Login</Text>
+      <TextInput style={styles.input} placeholder='Username' autoFocus/>
+      <TextInput style={styles.input} placeholder='Password' secureTextEntry/>
+      <Button style={styles.button} title='Submit' onPress={() => alert('Logged in!')}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  button: {
+    
+  },
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  input: {
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    margin: 5,
+  },
+
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 5,
+  }
 });
