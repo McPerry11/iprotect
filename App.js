9620163917e862a5336f0bf327c36cd5b75bf878
McPerry11 <mack.perry.co@gmail.com>
2024-02-17 19:20:21 +0800

    V0.2.0 - Added icons. Modified login layout.

--- App.js
+++ App.js
@@ -1,42 +1,111 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style='auto' />
+    <View style={[styles.appContainer, styles.border]}>
+      <StatusBar style='auto'/>
 
-      <Text style={styles.title}>Login</Text>
-      <TextInput style={styles.input} placeholder='Username' autoFocus/>
-      <TextInput style={styles.input} placeholder='Password' secureTextEntry/>
-      <Button style={styles.button} title='Submit' onPress={() => alert('Logged in!')}/>
+      <View style={[styles.login, styles.border]}>
+        <Text style={styles.title}>Login</Text>
+
+        <View style={styles.usernameContainer}>
+          <View style={[styles.icon, styles.inputIcon]}>
+            <Ionicons name='person' size={24}/>
+          </View>
+          <TextInput style={[styles.input, styles.username]} placeholder='Username'/>
+        </View>
+
+        <View style={styles.passwordContainer}>
+          <View style={[styles.icon, styles.inputIcon]}>
+            <Ionicons name='lock-closed' size={24}/>
+          </View>
+          <TextInput style={[styles.input, styles.password]} placeholder='Password'/>
+          <Pressable style={[styles.icon, styles.togglePassword]}>
+            <Ionicons name='eye' size={24} />
+          </Pressable>
+        </View>
+
+        <Text style={styles.error}>Error</Text>
+        <View style={styles.submit}>
+          <Button title='Submit'/>
+        </View>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  button: {
-    
-  },
-
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 15,
   },
 
-  input: {
-    height: 40,
+  border: {
     borderWidth: 1,
-    borderRadius: 10,
-    padding: 10,
-    margin: 5,
+    borderColor: 'red',
+  },
+  
+  error: {
+    color: 'red',
+    marginTop: 10,
+  },
+  
+  icon: {
+    justifyContent: 'center',
+    paddingHorizontal: 5,
+  },
+  
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    padding: 5,
   },
 
+  inputIcon: {
+    borderWidth: 1,
+    borderRightWidth: 0,
+  },
+  
+  login: {
+    width: '80%',
+  },
+
+  password: {
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+  },
+  
+  passwordContainer: {
+    flexDirection: 'row',
+    marginTop: 15,
+  },
+  
+  submit: {
+    marginTop: 20,
+  },
+  
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-    margin: 5,
-  }
+    textAlign: 'center',
+  },
+
+  togglePassword: {
+    borderWidth: 1,
+  },
+
+  username: {
+    borderLeftWidth: 0,
+  },
+
+  usernameContainer: {
+    flexDirection: 'row',
+    marginTop: 25,
+  },
 });
